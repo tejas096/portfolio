0cdc43c0c6749d86aced66e17b709b0f766c394e
Tejas Jain <tejasjain096@gmail.com>
2025-09-06 21:24:36 +0530

    Initial commit

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -8,26 +8,25 @@ type PropType = {
 };
 const SkillCard = ({ name, title, image, addClass }: PropType) => {
   return (
-    <Link to={"/skills"} className="w-full">
-      <div
-        className={`${
-          addClass && `${addClass}`
-        } bg-[rgb(39,40,41)] hover:bg-[rgb(145,75,241)] hover:cursor-pointer transition-all duration-300 ease-in-out items-center gap-[20px] justify-start h-[85px] lg:h-[100px] w-[450px] sm:w-[250px] xl:w-[320px] rounded-lg p-[15px] flex`}
-      >
-        <div className="flex justify-center items-center bg-white h-[70px] w-[70px] rounded-lg">
-          <img
-            src={image}
-            className="h-[70px] rounded-lg w-[70px] object-contain object-center"
-            loading="lazy"
-            alt={name}
-          />
-        </div>
-        <div className="text-[26px] font-semibold leading-[1em]">
-          {name} <br />
-          <span className="text-[rgb(217,217,217)] [line-height:0rem] text-[16px] font-normal">
-            {title}
-          </span>
-        </div>
+    <Link
+      to={"/skills"}
+      className={`${
+        addClass && `${addClass}`
+      } bg-[rgb(39,40,41)] hover:bg-[rgb(145,75,241)] hover:cursor-pointer transition-all duration-300 ease-in-out items-center gap-[20px] justify-start h-[85px] lg:h-[100px] w-[450px] sm:w-[250px] xl:w-[320px] rounded-lg p-[15px] flex`}
+    >
+      <div className="flex justify-center items-center bg-white h-[70px] w-[70px] rounded-lg">
+        <img
+          src={image}
+          className="h-[70px] rounded-lg w-[70px] object-contain object-center"
+          loading="lazy"
+          alt={name}
+        />
+      </div>
+      <div className="text-[26px] font-semibold leading-[1em]">
+        {name} <br />
+        <span className="text-[rgb(217,217,217)] [line-height:0rem] text-[16px] font-normal">
+          {title}
+        </span>
       </div>
     </Link>
   );
